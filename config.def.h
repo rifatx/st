adf7e44588fc99ec0e21221d67b2c79fc64be47e
rifatx <rifat.aricanli@gmail.com>
2023-06-28 19:41:29 +0300

    scroll key

--- config.def.h
+++ config.def.h
@@ -182,8 +182,8 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
-	{ ShiftMask,            Button4, kscrollup,      {.i = 1} },
-	{ ShiftMask,            Button5, kscrolldown,    {.i = 1} },
+	{ ShiftMask,            Button4, kscrollup,      {.i = 3} },
+	{ ShiftMask,            Button5, kscrolldown,    {.i = 3} },
 };
 
 /* Internal keyboard shortcuts. */
@@ -205,8 +205,8 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_Escape,      keyboard_select,{.i =  0} },
-	{ ControlMask,          XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ControlMask,          XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ ControlMask,          XK_Up,     kscrollup,      {.i = -1} },
+	{ ControlMask,          XK_Down,   kscrolldown,    {.i = -1} },
  };
 
 /*
