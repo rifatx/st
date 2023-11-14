8f57b24877cf1c637ccc8a76731a391e937a10d1
rifatx <rifat.aricanli@gmail.com>
2023-11-14 22:43:03 +0300

    trq shortcuts for zoom in/out

--- config.def.h
+++ config.def.h
@@ -196,8 +196,8 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Up,          zoom,           {.f = +2} },
-	{ TERMMOD,              XK_Down,        zoom,           {.f = -2} },
+	{ TERMMOD,              XK_semicolon,   zoom,           {.f = +2} },
+	{ TERMMOD,              XK_Iabovedot,   zoom,           {.f = -2} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
@@ -205,8 +205,8 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_Escape,      keyboard_select,{.i =  0} },
-	{ ControlMask,          XK_Up,     kscrollup,      {.i = -1} },
-	{ ControlMask,          XK_Down,   kscrolldown,    {.i = -1} },
+	{ ControlMask,          XK_Up,          kscrollup,      {.i = -1} },
+	{ ControlMask,          XK_Down,        kscrolldown,    {.i = -1} },
  };
 
 /*
